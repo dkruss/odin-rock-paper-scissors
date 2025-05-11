35bf406f8865b07577ce8ed5766897d98ebfbb96
David Kruss <davidis1337@gmail.com>
2025-05-11 20:43:30 +1000

    Add UI functionality

--- javascript.js
+++ javascript.js
@@ -23,63 +23,115 @@ function getHumanChoice() {
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
+    
+    let rockBtn = document.querySelector("#rock");
+    let paperBtn = document.querySelector("#paper");
+    let scissorsBtn = document.querySelector("#scissors");
+    let resetBtn = document.querySelector("#reset");
+
+    let playerScoreSpan = document.querySelector("#playerScore");
+    let computerScoreSpan = document.querySelector("#computerScore");
+    let gameLog = document.querySelector("div#gameLog");
+
+    let gameOver = false;
+
+    function reset() {
+        humanScore = 0;
+        computerScore = 0;
+        gameOver = false;
+        gameLog.textContent = "";
+        playerScoreSpan.textContent = humanScore;
+        computerScoreSpan.textContent = computerScore;  
+    }
 
     function playRound(humanChoice,computerChoice) {
         humanChoice = humanChoice.toLowerCase();
-        console.log(humanChoice);
-        if (humanChoice == computerChoice) {
-            console.log("Tie! Try again.");
-        } else {
+        
+        let gameLogEvent = document.createElement("p");
+
+        if (humanChoice == computerChoice && !gameOver) {
+            gameLogEvent.textContent = "Tie! 🤝 Try again.";
+            gameLog.appendChild(gameLogEvent);
+        } else if (!gameOver) {
             switch(humanChoice) {
                 case "rock":
                     if (computerChoice == "paper") {
-                        console.log("You lose! Paper beats rock.");
+                        gameLogEvent.textContent = "You lose! Paper beats rock.";
+                        gameLog.appendChild(gameLogEvent);
                         computerScore++;
                         break;
                     } else {
-                        console.log("You win! Rock beats scissors.");
+                        gameLogEvent.textContent = "You win! Rock beats scissors.";
+                        gameLog.appendChild(gameLogEvent);
                         humanScore++;
                         break;
                     }
                 case "paper":
                     if (computerChoice == "rock") {
-                        console.log("You win! Paper beats rock.");
+                        gameLogEvent.textContent = "You win! Paper beats rock.";
+                        gameLog.appendChild(gameLogEvent);
                         humanScore++;
                         break;
                     } else {
-                        console.log("You lose! Scissors beats paper.");
+                        gameLogEvent.textContent = "You lose! Scissors beats paper.";
+                        gameLog.appendChild(gameLogEvent);
                         computerScore++;
                         break;
                     }
                 case "scissors":
                     if (computerChoice == "rock") {
-                        console.log("You lose! Rock beats scissors.");
+                        gameLogEvent.textContent = "You lose! Rock beats scissors.";
+                        gameLog.appendChild(gameLogEvent);
                         computerScore++;
                         break;
                     } else {
-                        console.log("You win! Scissors beats paper.");
+                        gameLogEvent.textContent = "You win! Scissors beats paper.";
+                        gameLog.appendChild(gameLogEvent);
                         humanScore++;
                         break;
                     }
             }
+
+            if (humanScore == 5 || computerScore == 5) {
+                gameOver = true;
+                if (humanScore > computerScore) {
+                    gameLogEvent.textContent = "You win! 🎉";
+                    gameLog.appendChild(gameLogEvent);
+                } else if (humanScore < computerScore) {
+                    gameLogEvent.textContent = "You lose! 😭";
+                    gameLog.appendChild(gameLogEvent);
+                } else {
+                    gameLogEvent.textContent = "It's a tie! 🤝";
+                    gameLog.appendChild(gameLogEvent);
+                }
+            }
+        } else {
         }
+
+        playerScoreSpan.textContent = humanScore;
+        computerScoreSpan.textContent = computerScore; 
         
     }
 
-    for (let games = 0; games < 5; games++) {
-        playRound(getHumanChoice(),getComputerChoice());
-    }
+    rockBtn.addEventListener("click", () => {
+        playRound("rock",getComputerChoice());
+    });
 
-    console.log(`Your final score: ${humanScore}`);
-    console.log(`Computer's final score: ${computerScore}`);
-    
-    if (humanScore>computerScore) {
-        console.log("You win!");
-    } else if (humanScore < computerScore) {
-        console.log("You lose!");
-    } else {
-        console.log("It's a tie!");
-    }
+    paperBtn.addEventListener("click", () => {
+        playRound("paper",getComputerChoice());
+    });
+
+    scissorsBtn.addEventListener("click", () => {
+        playRound("scissors",getComputerChoice());
+    });
+
+    resetBtn.addEventListener("click", () => {
+        reset();
+    });
+
+    // for (let games = 0; games < 5; games++) {
+    //     playRound(getHumanChoice(),getComputerChoice());
+    // }
 }
 
 playGame();
